9ad0dd5d9218a3a4497c26cef911394a226a5bf3
Khuong <nguyendangkhuong96@gmail.com>
2022-08-04 20:20:59 +0700

    change api put

--- src/pages/api/product.ts
+++ src/pages/api/product.ts
@@ -47,13 +47,15 @@ async function handlePutRequest(req: NextApiRequest, res: NextApiResponse) {
       return res.status(422).send('Sản phẩm thiếu một hay nhiều mục')
     }
 
+  
+    await ProductModel.findByIdAndUpdate(_id, req.body, { new: true })
+
     //remove unessesary image
     const currentProduct: Product | null = await ProductModel.findById({ _id })
     const currentImagePublicId = currentProduct?.imagePublicId
     currentImagePublicId !== imagePublicId &&
       useRemoveImage(String(currentImagePublicId))
-
-    await ProductModel.findByIdAndUpdate(_id, req.body, { new: true })
+      
     res.status(200).send(`Sản phẩm đã được cập nhật!`)
   } catch (err) {
     console.log(err)
